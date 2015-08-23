9917c225a336b09c9fecff27dbe6e8b17593147e
Ice White <yashiro1899@gmail.com>
2015-08-23 16:00:55 +0800

    1440316855

--- test/index.js
+++ test/index.js
@@ -4,11 +4,14 @@ var start = process.hrtime();
 velocity.renderOnce("example/example.vm", {
     list: ["str", 43, null, true, false, {}, [], "中国"]
 }, function(err, data) {
-    console.log(process.hrtime(start));
+    var end = process.hrtime(start);
+
+    end = Math.round(end[0] * 1000 + end[1] / 1e6);
+    console.log("[spent %d ms]", end);
+
     if (err) {
         console.error(err);
         return;
     }
-
     console.log(data.toString());
 });
--- test/server.js
+++ test/server.js
@@ -6,12 +6,15 @@ setInterval(function() {
     velocity.render("example/example.vm", {
         list: ["str", 43, null, true, false, {}, [], "中国"]
     }, function(err, data) {
-        console.log(process.hrtime(start));
+        var end = process.hrtime(start);
+
+        end = Math.round(end[0] * 1000 + end[1] / 1e6);
+        console.log("[spent %d ms]", end);
+
         if (err) {
             console.error(err);
             return;
         }
-
         console.log(data.toString());
     });
 }, 1000);
