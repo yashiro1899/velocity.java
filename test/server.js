d8cbb112ede38647e63e3a43d5a0947719520db1
Ice White <yashiro1899@gmail.com>
2015-08-23 23:48:05 +0800

    1440344885

--- test/server.js
+++ test/server.js
@@ -7,14 +7,12 @@ setInterval(function() {
         list: ["str", 43, null, true, false, {}, [], "中国"]
     }, function(err, data) {
         var end = process.hrtime(start);
-
         end = Math.round(end[0] * 1000 + end[1] / 1e6);
-        console.log("[spent %d ms]", end);
 
         if (err) {
             console.error(err);
             return;
         }
-        console.log(data.toString());
+        console.log("%s[spent %d ms]", data.toString(), end);
     });
 }, 1000);
