65bf6cb2818bd93235d2cd431459ef4804e7fc8d
Ice White <yashiro1899@gmail.com>
2015-09-18 11:06:01 +0800

    1442545561

--- test/server.js
+++ test/server.js
@@ -3,9 +3,9 @@ var velocity = require("../");
 velocity.startServer();
 setInterval(function() {
     var start = process.hrtime();
-    velocity.render("example/example.vm", {
+    velocity.render("example.vm", {
         list: ["str", 43, null, true, false, {}, [], "中国"]
-    }, function(err, data) {
+    }, 'example', function(err, data) {
         var end = process.hrtime(start);
         end = Math.round(end[0] * 1000 + end[1] / 1e6);
 
