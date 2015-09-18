a20a916c1893f3552526e2d8788c61ce821686de
Ice White <yashiro1899@gmail.com>
2015-09-18 11:39:47 +0800

    1442547587

--- test/multiple.js
+++ test/multiple.js
@@ -6,9 +6,10 @@ var roots = ["example/a", "example/b", "example"];
 
 setInterval(function() {
     var start = process.hrtime();
+    var root = roots[i % 3];
     velocity.render("example.vm", {
         list: ["str", 43, null, true, false, {}, [], "中国"]
-    }, roots[i % 3], function(err, data) {
+    }, root, function(err, data) {
         var end = process.hrtime(start);
         end = Math.round(end[0] * 1000 + end[1] / 1e6);
 
@@ -17,6 +18,7 @@ setInterval(function() {
             return;
         }
         console.log("%s[spent %d ms]", data.toString(), end);
+        console.log("[root is '%s']\n\n--\n", root)
     });
 
     i += 1;
