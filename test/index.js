42a107afcfb3dca40d92f0001dbdf831c8005e9a
Ice White <yashiro1899@gmail.com>
2015-10-21 17:13:40 +0800

    1445418820

--- test/index.js
+++ test/index.js
@@ -1,64 +1,50 @@
-var velocity = require("../");
+'use strict';
 
-var start = process.hrtime();
-velocity.renderOnce("example.vm", {
-    list: ["str", 43, null, true, false, {}, [], "中国"]
-}, "example/", function(err, data) {
-    var end = process.hrtime(start);
-    end = Math.round(end[0] * 1000 + end[1] / 1e6);
+const velocity = require("../");
 
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("%s[spent %d ms]", data.toString(), end);
-    console.log("[velocity.renderOnce(filename, data, root, callback)]\n\n--\n");
-});
+var args = [
+    ["example.vm", {
+        list: ["str", 43, null, true, false, {}, [], "中国"]
+    }, "example/", "filename, data, root, callback"],
+    ["example/example.vm", {
+        list: ["str", 43, null, true, false, {}, [], "中国"]
+    }, "filename, data, callback"],
+    ["example.vm", "example/", "filename, root, callback"],
+    ["example/example.vm", "filename, callback"]
+];
+var cb = function () {
+    process.exit();
+};
 
-setTimeout(function() {
-var start = process.hrtime();
-velocity.renderOnce("example/example.vm", {
-    list: ["str", 43, null, true, false, {}, [], "中国"]
-}, function(err, data) {
-    var end = process.hrtime(start);
-    end = Math.round(end[0] * 1000 + end[1] / 1e6);
+function run(a, cb) {
+    var start = process.hrtime();
+    var message = a.pop();
 
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("%s[spent %d ms]", data.toString(), end);
-    console.log("[velocity.renderOnce(filename, data, callback)]\n\n--\n");
-});
-}, 2000);
+    a.push(function (err, data) {
+        var end = process.hrtime(start);
+        end = Math.round(end[0] * 1000 + end[1] / 1e6);
 
-setTimeout(function() {
-var start = process.hrtime();
-velocity.renderOnce("example.vm", "example", function(err, data) {
-    var end = process.hrtime(start);
-    end = Math.round(end[0] * 1000 + end[1] / 1e6);
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("%s[spent %d ms]", data.toString(), end);
+            console.log("[velocity.renderOnce(%s)]\n\n--\n", message);
+        }
+        cb(err);
+    });
+    velocity.renderOnce.apply(null, a);
+}
 
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("%s[spent %d ms]", data.toString(), end);
-    console.log("[velocity.renderOnce(filename, root, callback)]\n\n--\n");
-});
-}, 4000);
+args.reverse()
+    .forEach(function (a) {
+        cb = (function (cbb) {
+            return function (err) {
+                if (err) {
+                    return cbb(err);
+                }
+                run(a, cbb);
+            };
+        }(cb));
+    });
 
-setTimeout(function() {
-var start = process.hrtime();
-velocity.renderOnce("example/example.vm", {
-}, function(err, data) {
-    var end = process.hrtime(start);
-    end = Math.round(end[0] * 1000 + end[1] / 1e6);
-
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("%s[spent %d ms]", data.toString(), end);
-    console.log("[velocity.renderOnce(filename, callback)]\n\n--\n");
-});
-}, 6000);
+cb();
