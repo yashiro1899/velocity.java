5d3ae5a8d3cdb3531e14dce9e9b6cd170f7e45d4
Ice White <yashiro1899@gmail.com>
2015-10-21 17:51:08 +0800

    1445421068

--- test/server.js
+++ test/server.js
@@ -18,24 +18,26 @@ var cb = function () {
 
 function run(a, cb) {
     var start = process.hrtime();
-    var message = a.pop();
+    var message = a[a.length - 1];
 
-    a.push(function (err, data) {
-        var end = process.hrtime(start);
-        end = Math.round(end[0] * 1000 + end[1] / 1e6);
+    velocity.render.apply(null, a.slice(0, -1)
+        .concat([function (err, data) {
+            var end = process.hrtime(start);
+            end = Math.round(end[0] * 1000 + end[1] / 1e6);
 
-        if (err) {
-            console.error(err);
-        } else {
-            console.log("%s[spent %d ms]", data.toString(), end);
-            console.log("[velocity.renderOnce(%s)]\n\n--\n", message);
-        }
-        cb(err);
-    });
-    velocity.render.apply(null, a);
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("%s[spent %d ms]", data.toString(), end);
+                console.log("[velocity.render(%s)]\n\n--\n", message);
+            }
+            cb(err);
+    }]));
 }
 
 velocity.startServer();
+args = args.concat(args);
+args = args.concat(args);
 args.reverse()
     .forEach(function (a) {
         cb = (function (cbb) {
