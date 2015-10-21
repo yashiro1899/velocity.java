8b56c5893e63e3183d95d1dd653805d3bf4ba852
Ice White <yashiro1899@gmail.com>
2015-10-21 16:48:31 +0800

    1445417311

--- test/multiple.js
+++ test/multiple.js
@@ -1,26 +1,42 @@
+'use strict';
+
 var velocity = require("../");
-velocity.startServer();
 
-var i = 0;
 var roots = ["example/a", "example/b", "example"];
+var cb = function () {
+    process.exit();
+};
 
-setInterval(function() {
+function run(root, cb) {
     var start = process.hrtime();
-    var root = roots[i % 3];
     velocity.render("example.vm", {
         list: ["str", 43, null, true, false, {}, [], "中国"]
-    }, root, function(err, data) {
+    }, root, function (err, data) {
         var end = process.hrtime(start);
         end = Math.round(end[0] * 1000 + end[1] / 1e6);
-
         if (err) {
             console.error(err);
-            return;
+        } else {
+            console.log("%s[spent %d ms]", data.toString(), end);
+            console.log("[root is '%s']\n--\n", root);
         }
-        console.log("%s[spent %d ms]", data.toString(), end);
-        console.log("[root is '%s']\n\n--\n", root)
+        cb(err);
+    });
+}
+
+velocity.startServer();
+roots = roots.concat(roots);
+roots = roots.concat(roots);
+roots.reverse()
+    .forEach(function (root) {
+        cb = (function (cbb) {
+            return function (err) {
+                if (err) {
+                    return cbb(err);
+                }
+                run(root, cbb);
+            };
+        }(cb));
     });
 
-    i += 1;
-    if (i > 9) process.exit();
-}, 1000);
+cb();
